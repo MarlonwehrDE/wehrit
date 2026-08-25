4ec7884efc6b03ab4856c38001417019d432d661
Marlon Wehr <marlon@marlonwehr.de>
2026-08-25 14:33:36 +0200

    Remove sitemap integration (compatibility issue)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://wehrit.de',
-  integrations: [sitemap()],
+  site: 'https://wehr-it.de',
 });
